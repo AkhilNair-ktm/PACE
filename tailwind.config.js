c6dda09f8dbff55cc3a23530d9a796c5efb1c335
Shaun MR <shaunmr009@gmail.com>
2025-01-18 20:54:58 +0530

    test commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,27 @@ module.exports = {
       xl: "1180px",
     },
     extend: {},
+    keyframes: {
+      move: {
+        "50%": { transform: "translateY(-1rem)" },
+      },
+      scaleup: {
+        "0%": { transform: "scale(0.8)" },
+        "50%": { transform: "scale(1)" },
+        "100%": { transform: "scale(0.8)" },
+      },
+    },
+    animation: {
+      movingY: "move 3s linear infinite",
+      scaleup: "scaleup 3s linear infinite",
+    },
     fontFamily: {
       Anton: ["Anton", "sans-serif"], // Correct fallback
       PermanentMarker: ["Permanent Marker", "sans-serif"], // Correct fallback
       Monoton: ["Monoton", "sans-serif"], // Correct fallback
       Mogra: ["Mogra", "sans-serif"], // Correct fallback
-      Rubik80: ["Rubik 80s Fade", "sans-serif"], 
-      Jost :["Jost","serif"]
+      Rubik80: ["Rubik 80s Fade", "sans-serif"],
+      Jost: ["Jost", "serif"],
     },
     container: {
       center: true,
